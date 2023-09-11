975887f89b5cab1a87905d342d23ca4df892f7a1
newayer <cn_gwq@sina.com>
2023-09-11 12:07:55 +0800

    package/autossh: add init scripts

--- package/autossh/autossh.mk
+++ package/autossh/autossh.mk
@@ -14,4 +14,14 @@ AUTOSSH_AUTORECONF = YES
 
 AUTOSSH_CONF_OPTS = --with-ssh=/usr/bin/ssh
 
+define AUTOSSH_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D package/autossh/S86autossh \
+		$(TARGET_DIR)/etc/init.d/S86autossh
+endef
+
+define AUTOSSH_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/autossh/autossh.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/autossh.service
+endef
+
 $(eval $(autotools-package))
