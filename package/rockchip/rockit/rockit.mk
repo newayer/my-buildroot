601931df54b78b3a902dc54f2d96c00b65a3a168
ZiHan Huang <zack.huang@rock-chips.com>
2024-06-24 17:05:43 +0800

    package: rockit: fix rockit depend
    Change-Id: I03ad449905e750cd9e57a1e874645444a1ae1515
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>

--- package/rockchip/rockit/rockit.mk
+++ package/rockchip/rockit/rockit.mk
@@ -10,14 +10,18 @@ ROCKIT_SITE_METHOD = local
 
 ROCKIT_INSTALL_STAGING = YES
 
-ROCKIT_DEPENDENCIES = rockchip-mpp rockchip-rga alsa-lib
-
 ifneq ($(BR2_PACKAGE_RK3308),)
 ROCKIT_CONF_OPTS += -DRK3308=TRUE
+ROCKIT_DEPENDENCIES = alsa-lib
 endif
 
 ifneq ($(BR2_PACKAGE_RK3506),)
 ROCKIT_CONF_OPTS += -DRK3506=TRUE
+ROCKIT_DEPENDENCIES = rockchip-rga alsa-lib
+endif
+
+ifeq ($(BR2_PACKAGE_RK3506)$(BR2_PACKAGE_RK3308),)
+ROCKIT_DEPENDENCIES = rockchip-mpp rockchip-rga alsa-lib
 endif
 
 $(eval $(cmake-package))
