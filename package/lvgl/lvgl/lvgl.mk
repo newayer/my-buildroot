986e02e26975307a480bb418babd38e3c2c032aa
Jiajian Wu <jair.wu@rock-chips.com>
2024-06-28 15:15:19 +0800

    package: lvgl: Prefer using Kconfig from LVGL team
    Change-Id: Ie6cc2ae276ee049b39ca8e4f5bc810bdd5cd0052
    Signed-off-by: Jiajian Wu <jair.wu@rock-chips.com>

--- package/lvgl/lvgl/lvgl.mk
+++ package/lvgl/lvgl/lvgl.mk
@@ -8,30 +8,45 @@ LVGL_VERSION = 8.3.0
 LVGL_SITE = $(call github,lvgl,lvgl,v$(LVGL_VERSION))
 LVGL_INSTALL_STAGING = YES
 
+ifeq ($(BR2_LV_USE_DEMO_WIDGETS), y)
+LVGL_CONF_OPTS += -DLV_USE_DEMO_WIDGETS=1
+endif
+
+ifeq ($(BR2_LV_USE_DEMO_KEYPAD_AND_ENCODER), y)
+LVGL_CONF_OPTS += -DLV_USE_DEMO_KEYPAD_AND_ENCODER=1
+endif
+
+ifeq ($(BR2_LV_USE_DEMO_BENCHMARK), y)
+LVGL_CONF_OPTS += -DLV_USE_DEMO_BENCHMARK=1
+endif
+
+ifeq ($(BR2_LV_USE_DEMO_STRESS), y)
+LVGL_CONF_OPTS += -DLV_USE_DEMO_STRESS=1
+endif
+
+ifeq ($(BR2_LV_USE_DEMO_MUSIC), y)
+LVGL_CONF_OPTS += -DLV_USE_DEMO_MUSIC=1
+endif
+
+ifeq ($(BR2_LV_USE_FREETYPE), y)
 LVGL_DEPENDENCIES += freetype
-
-ifeq ($(BR2_PACKAGE_LVGL_DEMOS), y)
-LVGL_CONF_OPTS += -DLV_USE_DEMO_WIDGETS=1	\
-	-DLV_USE_DEMO_KEYPAD_AND_ENCODER=1	\
-	-DLV_USE_DEMO_BENCHMARK=1	\
-	-DLV_USE_DEMO_STRESS=1	\
-	-DLV_USE_DEMO_MUSIC=1
 endif
 
-ifeq ($(BR2_PACKAGE_LVGL_DEMO_CUSTOM), y)
-LVGL_CONF_OPTS += -DLV_USE_DEMO_CUSTOM=1
-endif
-
-ifeq ($(BR2_PACKAGE_LVGL_USE_SDL), y)
+ifeq ($(BR2_LV_USE_GPU_SDL), y)
 LVGL_CONF_OPTS += -DLV_USE_GPU_SDL=1
 LVGL_DEPENDENCIES += sdl2
 endif
 
-LVGL_CONF_OPTS += -DLV_COLOR_DEPTH=$(BR2_PACKAGE_LVGL_COLOR_DEPTH)
-ifeq ($(BR2_PACKAGE_LVGL_COLOR_16_SWAP), y)
-LVGL_CONF_OPTS += -DLV_COLOR_16_SWAP=1
-else
-LVGL_CONF_OPTS += -DLV_COLOR_16_SWAP=0
-endif
+define LVGL_PRE_HOOK
+	echo "#ifndef LV_CONF_H" > $(LVGL_DIR)/lv_conf.h
+	echo "#define LV_CONF_H" >> $(LVGL_DIR)/lv_conf.h
+	cat $(BR2_CONFIG) | grep "LVGL\|LV_" | grep -v "#" | grep -v "LV_LOG_LEVEL_" | \
+		grep -v "LV_STDLIB_" | sed -e "s/BR2_//g" | \
+		sed -e "s/LV_FONT_DEFAULT_/CONFIG_LV_FONT_DEFAULT_/g" | \
+		sed -e "s/=y/ 1/g" | sed -e "s/=/ /g" | sed -e "s/^/#define /" >> \
+		$(LVGL_DIR)/lv_conf.h
+	echo "#endif" >> $(LVGL_DIR)/lv_conf.h
+endef
+LVGL_PRE_CONFIGURE_HOOKS += LVGL_PRE_HOOK
 
 $(eval $(cmake-package))
