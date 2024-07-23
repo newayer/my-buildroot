dbfc4efcc2f2578653c2d7c7bb6ae16c6f078f34
newayer <cn_gwq@sina.com>
2024-07-23 17:35:18 +0800

    vendor/package/python-thingsboard-gateway: 完善启动脚本

--- vendor/package/python-thingsboard-gateway/python-thingsboard-gateway.mk
+++ vendor/package/python-thingsboard-gateway/python-thingsboard-gateway.mk
@@ -11,4 +11,14 @@ PYTHON_THINGSBOARD_GATEWAY_SETUP_TYPE = setuptools
 PYTHON_THINGSBOARD_GATEWAY_LICENSE = Apache-2.0
 PYTHON_THINGSBOARD_GATEWAY_LICENSE_FILES = LICENSE
 
+define PYTHON_THINGSBOARD_GATEWAY_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D $(PYTHON_THINGSBOARD_GATEWAY_PKGDIR)S90thingsboard-gateway $(TARGET_DIR)/etc/init.d/S90thingsboard-gateway
+	$(INSTALL) -m 755 -D $(PYTHON_THINGSBOARD_GATEWAY_PKGDIR)daemon.sh $(TARGET_DIR)/usr/sbin/daemon-app.sh
+	$(INSTALL) -m 755 -D $(PYTHON_THINGSBOARD_GATEWAY_PKGDIR)S99daemon-thingsboard-gateway $(TARGET_DIR)/etc/init.d/S99daemon-thingsboard-gateway
+endef
+
+define PYTHON_THINGSBOARD_GATEWAY_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(PYTHON_THINGSBOARD_GATEWAY_PKGDIR)thingsboard-gateway.service $(TARGET_DIR)/usr/lib/systemd/system/thingsboard-gateway.service
+endef
+
 $(eval $(python-package))
