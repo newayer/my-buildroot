e0ef46503506e06674b935670cf894740629e0c9
Zain Wang <wzz@rock-chips.com>
2024-09-04 14:48:50 +0800

    board: rk3506: add fast-display-overlay
    Move the startup of rk_demo as early as possible.
    
    Change-Id: Iab062ea6b294ee71c0510679c61c5cd2fdf326f8
    Signed-off-by: Zain Wang <wzz@rock-chips.com>

---
+++ board/rockchip/rk3506/post-build-fast-display.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+TARGET=$1
+
+SH_LV_DEMO=$(ls $TARGET/etc/init.d/S* | grep lv_demo || true)
+SH_ASYNC_COMMIT=$(ls $TARGET/etc/init.d/S* | grep sync-commit || true)
+
+mkdir -p $TARGET/etc/init.d/pre_init
+
+for i in "${SH_LV_DEMO}" "${SH_ASYNC_COMMIT}" ; do
+	if [ -n "$i" ]; then
+		mv $i $TARGET/etc/init.d/pre_init/.
+	fi
+done
