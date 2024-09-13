8dfc0dca5f0f6ee9d1dd5a2d5e2d8eb9c2416fd8
newayer <cn_gwq@sina.com>
2024-09-13 16:45:59 +0800

    vendor：优化uav pppd daemon进程实现

--- vendor/package/worksite-d/daemon-uav.sh
+++ vendor/package/worksite-d/daemon-uav.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+function pppd_restart()
+{
+    echo "`date "+%Y-%m-%d %H:%M:%S"`: pppd_daemon restart..." >> /opt/logs/main_error.log
+	kill `pidof pppd`
+    /etc/init.d/S45pppd restart >> /opt/logs/main_error.log
+}
+
+function pppd_exist()
+{
+    for i in `pidof python3`;
+    do
+        app=`grep pppd_daemon.py /proc/$i/cmdline`
+        if [ "$app" ] ; then
+            return 1
+        fi
+    done
+    return 0
+}
+
 function app_restart()
 {
     echo "`date "+%Y-%m-%d %H:%M:%S"`: main app restart..." >> /opt/logs/main_error.log
@@ -24,7 +43,13 @@ while :
 do
     sleep 3
 
-    app_exist
+    pppd_exist
+    if [ $? -eq 0 ]; then
+        pppd_restart
+        sleep 3
+    fi
+
+	app_exist
     if [ $? -eq 0 ]; then
         app_restart
         sleep 3
