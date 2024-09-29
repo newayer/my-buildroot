f5c2b99629394a82ff3a2793676a313e5906f13f
newayer <cn_gwq@sina.com>
2024-09-29 18:23:32 +0800

    vendor: ultra-w支持静态IP网卡

--- vendor/board/newayer/common/post-build.sh
+++ vendor/board/newayer/common/post-build.sh
@@ -16,18 +16,3 @@ chmod 0600 $TARGET_DIR/root/.ssh/id_rsa
 chmod 0644 $TARGET_DIR/root/.ssh/id_rsa.pub
 chmod 0644 $TARGET_DIR/root/.ssh/authorized_keys
 chmod 0644 $TARGET_DIR/root/.ssh/config
-
-if [ -e $TARGET_DIR/etc/network/interfaces ]; then
-  echo "" >> $TARGET_DIR/etc/network/interfaces
-  echo "auto eth0" >> $TARGET_DIR/etc/network/interfaces
-  echo "iface eth0 inet static" >> $TARGET_DIR/etc/network/interfaces
-  echo "	address 192.168.117.136/24" >> $TARGET_DIR/etc/network/interfaces
-  echo "	gateway 192.168.117.1" >> $TARGET_DIR/etc/network/interfaces
-
-  echo "" >> $TARGET_DIR/etc/network/interfaces
-  echo "auto eth1" >> $TARGET_DIR/etc/network/interfaces
-  echo "iface eth1 inet static" >> $TARGET_DIR/etc/network/interfaces
-  echo "	address 192.168.118.136/24" >> $TARGET_DIR/etc/network/interfaces
-  echo "	gateway 192.168.118.1" >> $TARGET_DIR/etc/network/interfaces
-
-fi
