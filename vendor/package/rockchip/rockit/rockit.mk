826fcf732f6fce52c7973d445f96d6303ce28ad6
newayer <cn_gwq@sina.com>
2024-10-28 18:30:55 +0800

    vendor：rockchip kernel module，解决模块没有被编译的问题
        同步luckfox-pico库中提交的以下记录
    
        commit e1caff31f64cbb1e7dbed793b764bbf1c020d6c7
        Merge: bcec08c39 b41667589
        Author: luckfox-eng33 <144299491+luckfox-eng33@users.noreply.github.com>
        Date:   Tue Oct 15 11:36:34 2024 +0800
    
            Merge pull request #180 from luckfox-eng29/pullrequest
    
            Partial bug fixes and optimization

--- vendor/package/rockchip/rockit/rockit.mk
+++ vendor/package/rockchip/rockit/rockit.mk
@@ -12,5 +12,12 @@ ROCKIT_MODULE_MAKE_OPTS = KERNEL_DIR=$(LINUX_DIR) \
 			  CONFIG_ROCKCHIP_RV1106_ROCKIT=m \
 			  KBUILD_EXTRA_SYMBOLS=$(KMPP_DIR)/Module.symvers
 
+define ROCKIT_INSTALL_FIRMWARE
+	$(INSTALL) -d $(TARGET_DIR)/lib/firmware
+	$(INSTALL) -D -m 0644 $(@D)/hpmcu_wrap.bin $(TARGET_DIR)/lib/firmware/hpmcu_wrap.bin
+endef
+
+ROCKIT_POST_INSTALL_TARGET_HOOKS += ROCKIT_INSTALL_FIRMWARE
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
