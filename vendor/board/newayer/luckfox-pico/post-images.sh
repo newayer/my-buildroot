17870d1770cd569b5c1530c6c177c6d1136ccbcf
newayer <cn_gwq@sina.com>
2025-05-28 21:31:22 +0800

    vendor：移除不再支持的项目配置

--- vendor/board/newayer/luckfox-pico/post-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -fr $BINARIES_DIR/rootfs.img && ln -s rootfs.ubi $BINARIES_DIR/rootfs.img
-
--- vendor/board/newayer/zlg-paver/local-paver.mk
+++
@@ -1,4 +0,0 @@
-#LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../my-kernel
-#UBOOT_OVERRIDE_SRCDIR = $(TOPDIR)/../my-uboot
-
-#WORKSITE_D_OVERRIDE_SRCDIR = $(TOPDIR)/../worksite-d
