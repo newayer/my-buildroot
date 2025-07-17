f94814306a8502f74581fc1822e5148e8babbf68
newayer <cn_gwq@sina.com>
2025-07-17 18:42:56 +0800

    init-scripts：修改wifi监控程序判断逻辑

--- vendor/package/init-scripts/files/daemon-wifi.sh
+++ vendor/package/init-scripts/files/daemon-wifi.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
+function ping_test() {
+    for n in {1 2 3 4 5};
+    do
+        ping -c 3 -W 3 www.baidu.com > /dev/null 2>&1
+        if [ $? = 0 ] ; then
+            return 0
+        fi
+        sleep 3
+    done
+
+    return -1
+}
+
 while :
 do
-	sleep 9
+	sleep 30
 
     wpaIsRun=$(ps -ef |grep "wpa_supplicant " |grep -v "grep")
     if [ "$wpaIsRun" ] ; then
         IP=$(ifconfig wlan0 | grep "inet addr:" | tr -d A-z: | awk {'print $1'})
         if [ "$IP" ] ; then
-            curl --connect-timeout 5 -I www.baidu.com > /dev/null 2>&1
+            ping_test
             if [ $? = 0 ] ; then
                 continue
             fi
@@ -17,12 +30,4 @@ do
 
     /etc/init.d/S40network restart
 
-    for((i=1;i<=10;i++));
-	do
-		IP=$(ifconfig wlan0 | grep "inet addr:" | tr -d A-z: | awk {'print $1'})
-		if [ "$IP" ] ; then
-			break
-		fi
-		sleep 6
-	done
 done
