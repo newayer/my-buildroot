739b9bb14b5e357f7988719f78697049b4c4268b
newayer <cn_gwq@sina.com>
2026-07-09 10:09:40 +0800

    pppd-scripts：优化modem拨号脚本实现

--- vendor/package/pppd-scripts/pppd-scripts.mk
+++ vendor/package/pppd-scripts/pppd-scripts.mk
@@ -9,20 +9,15 @@ PPPD_SCRIPTS_SITE = $(PPPD_SCRIPTS_PKGDIR)files
 PPPD_SCRIPTS_SITE_METHOD = local
 
 define PPPD_SCRIPTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -D $(PPPD_SCRIPTS_PKGDIR)files/sysctl.conf $(TARGET_DIR)/etc/sysctl.conf
-	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/chat-wcdma-connect $(TARGET_DIR)/etc/ppp/peers/chat-wcdma-connect
-	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/chat-wcdma-disconnect $(TARGET_DIR)/etc/ppp/peers/chat-wcdma-disconnect
-	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/ppp-wcdma $(TARGET_DIR)/etc/ppp/peers/ppp_options
+	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/chat-5g-connect $(TARGET_DIR)/etc/ppp/peers/chat-5g-connect
+	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/chat-5g-disconnect $(TARGET_DIR)/etc/ppp/peers/chat-5g-disconnect
+	$(INSTALL) -m 644 -D $(PPPD_SCRIPTS_PKGDIR)files/ppp/peers/ppp_options $(TARGET_DIR)/etc/ppp/peers/ppp_options
 endef
 
 define PPPD_SCRIPTS_INSTALL_INIT_SYSV
 	$(INSTALL) -m 755 -D $(PPPD_SCRIPTS_PKGDIR)files/S45pppd $(TARGET_DIR)/etc/init.d/S45pppd
 	$(INSTALL) -m 755 -D $(PPPD_SCRIPTS_PKGDIR)files/S99daemon-pppd $(TARGET_DIR)/etc/init.d/S99daemon-pppd
 	$(INSTALL) -m 755 -D $(PPPD_SCRIPTS_PKGDIR)files/daemon-pppd.sh $(TARGET_DIR)/usr/sbin/daemon-pppd.sh
-endef
-
-define PPPD_SCRIPTS_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 $(PPPD_SCRIPTS_PKGDIR)files/pppd.service $(TARGET_DIR)/usr/lib/systemd/system/pppd.service
 endef
 
 $(eval $(generic-package))
